11162d850624b63f39e9d7e5c98ee03b0543d2ee
weizhangCoder <475740630@qq.com>
2017-12-18 11:15:40 +0800

    添加UIButton的类别方法

--- ZWTakeYouFly/Classes/Tool/Category/UIKit/UIButton/UIButton+fast.h
+++ ZWTakeYouFly/Classes/Tool/Category/UIKit/UIButton/UIButton+fast.h
@@ -7,7 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
-
+typedef void(^tapActionBlock)(UIButton *button);
 @interface UIButton (fast)
+
+@property(nonatomic,copy)tapActionBlock actionBlock;
+
+/**
+ 通过block对button的点击事件封装
+ 
+ @param frame       frame
+ @param title       标题
+ @param bgImageName 背景图片
+ @param actionBlock 点击事件回调block
+ 
+ @return button
+ */
++ (UIButton *)createBtnFrame:(CGRect)frame title:(NSString *)title bgImageName:(NSString *)bgImageName action:(tapActionBlock)actionBlock;
+
 + (UIButton *)buttonWithTitle:(NSString *)title andTitleColor:(UIColor *)titleColor;
+
+//** 可以自定义图片和标题间的间隔 */
+- (void)setIconInLeftWithSpacing:(CGFloat)Spacing;
+- (void)setIconInRightWithSpacing:(CGFloat)Spacing;
+- (void)setIconInTopWithSpacing:(CGFloat)Spacing;
+- (void)setIconInBottomWithSpacing:(CGFloat)Spacing;
+
 @end
--- ZWTakeYouFly/Classes/Tool/Category/UIKit/UIButton/UIButton+fast.m
+++ ZWTakeYouFly/Classes/Tool/Category/UIKit/UIButton/UIButton+fast.m
@@ -7,8 +7,63 @@
 //
 
 #import "UIButton+fast.h"
+#import <objc/runtime.h>
 
 @implementation UIButton (fast)
+static NSString *keyOfUseCategoryMethod;//用分类方法创建的button，关联对象的key
+static NSString *keyOfBlock;
+
++ (UIButton *)createBtnFrame:(CGRect)frame title:(NSString *)title bgImageName:(NSString *)bgImageName action:(tapActionBlock)actionBlock
+{
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = frame;
+    [button setTitle:title forState:UIControlStateNormal];
+    [button setBackgroundImage:[UIImage imageNamed:bgImageName] forState:UIControlStateNormal];
+    [button addTarget:button action:@selector(tapAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    /**
+     *用runtime中的函数通过key关联对象
+     *
+     *objc_setAssociatedObject(id object, const void *key, id value, objc_AssociationPolicy policy)
+     *id object                     表示关联者，是一个对象，变量名理所当然也是object
+     *const void *key               获取被关联者的索引key
+     *id value                      被关联者，这里是一个block
+     *objc_AssociationPolicy policy 关联时采用的协议，有assign，retain，copy等协议，一般使用OBJC_ASSOCIATION_RETAIN_NONATOMIC
+     
+     */
+    objc_setAssociatedObject (button , &keyOfUseCategoryMethod , actionBlock, OBJC_ASSOCIATION_COPY_NONATOMIC );
+    
+    return button;
+}
+- (void)tapAction:(UIButton*)sender
+{
+    /**
+     * 通过key获取被关联对象
+     *objc_getAssociatedObject(id object, const void *key)
+     *
+     */
+    tapActionBlock block = ( tapActionBlock )objc_getAssociatedObject (sender , &keyOfUseCategoryMethod );
+    
+    if (block) {
+        
+        block(sender);
+        
+    }
+}
+
+
+
+- (void)setActionBlock:(tapActionBlock)actionBlock
+{
+    objc_setAssociatedObject (self , &keyOfBlock , actionBlock, OBJC_ASSOCIATION_COPY_NONATOMIC );
+    
+}
+
+- (tapActionBlock)actionBlock
+{
+    return objc_getAssociatedObject (self , &keyOfBlock );
+}
 
 + (UIButton *)buttonWithTitle:(NSString *)title andTitleColor:(UIColor *)titleColor{
     UIButton *btn = [UIButton buttonWithType:0];
@@ -25,6 +80,86 @@
     btn.layer.cornerRadius = 5;
     btn.layer.masksToBounds = YES;
     return btn;
+}
+- (void)setIconInLeftWithSpacing:(CGFloat)Spacing
+{
+    self.titleEdgeInsets = (UIEdgeInsets){
+        .top    = 0,
+        .left   = Spacing/2,
+        .bottom = 0,
+        .right  = -Spacing/2,
+    };
+    
+    self.imageEdgeInsets = (UIEdgeInsets){
+        .top    = 0,
+        .left   = -Spacing/2,
+        .bottom = 0,
+        .right  = Spacing/2,
+    };
+}
+
+- (void)setIconInRightWithSpacing:(CGFloat)Spacing
+{
+    CGFloat img_W = self.imageView.frame.size.width;
+    CGFloat tit_W = self.titleLabel.frame.size.width;
+    
+    self.titleEdgeInsets = (UIEdgeInsets){
+        .top    = 0,
+        .left   = - (img_W + Spacing / 2),
+        .bottom = 0,
+        .right  =   (img_W + Spacing / 2),
+    };
+    
+    self.imageEdgeInsets = (UIEdgeInsets){
+        .top    = 0,
+        .left   =   (tit_W + Spacing / 2),
+        .bottom = 0,
+        .right  = - (tit_W + Spacing / 2),
+    };
+}
+
+- (void)setIconInTopWithSpacing:(CGFloat)Spacing
+{
+    CGFloat img_W = self.imageView.frame.size.width;
+    CGFloat img_H = self.imageView.frame.size.height;
+    CGFloat tit_W = self.titleLabel.frame.size.width;
+    CGFloat tit_H = self.titleLabel.frame.size.height;
+    
+    self.titleEdgeInsets = (UIEdgeInsets){
+        .top    =   (tit_H / 2 + Spacing / 2),
+        .left   = - (img_W / 2),
+        .bottom = - (tit_H / 2 + Spacing / 2),
+        .right  =   (img_W / 2),
+    };
+    
+    self.imageEdgeInsets = (UIEdgeInsets){
+        .top    = - (img_H / 2 + Spacing / 2),
+        .left   =   (tit_W / 2),
+        .bottom =   (img_H / 2 + Spacing / 2),
+        .right  = - (tit_W / 2),
+    };
+}
+
+- (void)setIconInBottomWithSpacing:(CGFloat)Spacing
+{
+    CGFloat img_W = self.imageView.frame.size.width;
+    CGFloat img_H = self.imageView.frame.size.height;
+    CGFloat tit_W = self.titleLabel.frame.size.width;
+    CGFloat tit_H = self.titleLabel.frame.size.height;
+    
+    self.titleEdgeInsets = (UIEdgeInsets){
+        .top    = - (tit_H / 2 + Spacing / 2),
+        .left   = - (img_W / 2),
+        .bottom =   (tit_H / 2 + Spacing / 2),
+        .right  =   (img_W / 2),
+    };
+    
+    self.imageEdgeInsets = (UIEdgeInsets){
+        .top    =   (img_H / 2 + Spacing / 2),
+        .left   =   (tit_W / 2),
+        .bottom = - (img_H / 2 + Spacing / 2),
+        .right  = - (tit_W / 2),
+    };
 }
 //
 //static UIButton *createResultLabel() {
